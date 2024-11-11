aaba478c4daa195b3e2b184bc282f0c0bee3a599
DevRamona <r.ingabire@alustudent.com>
2024-11-11 11:01:35 +0200

    required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { LearningPathsModule } from './learning-paths/learning-paths.module';
+import { JobsModule } from './jobs/jobs.module';
+import { CoursesModule } from './courses/courses.module';
+import { UsersModule } from './users/users.module';
+import { AuthnestService } from './g/authnest/authnest.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CoursesModule, JobsModule, LearningPathsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthnestService],
 })
 export class AppModule {}
