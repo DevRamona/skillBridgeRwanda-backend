298c319918c0bb4608fa2cd7edfa7d7de8b41d25
DevRamona <r.ingabire@alustudent.com>
2024-11-28 09:25:48 +0200

    update: frontend uri

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['https://skillbridgeramona.netlify.app', 'http://localhost:3002'],
+    origin: ['https://skillbridgerwanda.onrender.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
